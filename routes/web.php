d78710e596aa9ec889b279f52e01b2f058b8fdfd
Awaidus <m.awaidus@gmail.com>
2019-08-25 14:43:13 +0500

    Add auth middleware to all routes

--- routes/web.php
+++ routes/web.php
@@ -2,38 +2,43 @@
 
 Auth::routes();
 
-Route::get('/', function () {
-    return view('dashboard.index');
-})->name('home');
+// Apply auth middleware to all routes. 
+// Now only authorize person can access the site 
+Route::middleware(['auth'])->group(function () {
 
-// Appraoch-1 to draw the routes
-Route::get('/parents', 'ParentsController@index')->name('parents.index');
-Route::get('/parents/create', 'ParentsController@create')->name('parents.create');
-Route::get('/parents/{parent}', 'ParentsController@show')->name('parents.show'); //'{parent]' here is a variable/ wildcard
-Route::get('/parents/{parent}/edit', 'ParentsController@edit')->name('parents.edit');
-Route::post('/parents', 'ParentsController@store')->name('parents.store');
-Route::patch('/parents/{parent}', 'ParentsController@update')->name('parents.update');
-Route::delete('/parents/{parent}', 'ParentsController@destroy')->name('parents.destroy');
+    Route::get('/', function () {
+        return view('dashboard.index');
+    })->name('home');
 
-// Approach-2 to draw the routes
-// This will generate all the routes resources automatically like index, create, show, edit, update, delet
-Route::resource('students', 'StudentsController');
+    // Appraoch-1 to draw the routes
+    Route::get('/parents', 'ParentsController@index')->name('parents.index');
+    Route::get('/parents/create', 'ParentsController@create')->name('parents.create');
+    Route::get('/parents/{parent}', 'ParentsController@show')->name('parents.show'); //'{parent]' here is a variable/ wildcard
+    Route::get('/parents/{parent}/edit', 'ParentsController@edit')->name('parents.edit');
+    Route::post('/parents', 'ParentsController@store')->name('parents.store');
+    Route::patch('/parents/{parent}', 'ParentsController@update')->name('parents.update');
+    Route::delete('/parents/{parent}', 'ParentsController@destroy')->name('parents.destroy');
 
-Route::resource('classes', 'ClassesController');
+    // Approach-2 to draw the routes
+    // This will generate all the routes resources automatically like index, create, show, edit, update & delete 
+    Route::resource('students', 'StudentsController');
 
-Route::resource('attendances', 'AttendancesController');
+    Route::resource('classes', 'ClassesController');
 
-Route::get('/missing-attendances', 'MissingAttendancesController@index')->name('missing-attendances.index');
-Route::post('/missing-attendances/mail', 'MissingAttendancesController@mail')->name('missing-attendances.mail');
+    Route::resource('attendances', 'AttendancesController');
 
-Route::get('/approved-attendances', 'ApprovedAttendancesController@index')->name('approved-attendances.index');
-Route::post('/missing-attendances/{attendance}/approved', 'ApprovedAttendancesController@store')->name('approved-attendances.store');
+    Route::get('/missing-attendances', 'MissingAttendancesController@index')->name('missing-attendances.index');
+    Route::post('/missing-attendances/mail', 'MissingAttendancesController@mail')->name('missing-attendances.mail');
+
+    Route::get('/approved-attendances', 'ApprovedAttendancesController@index')->name('approved-attendances.index');
+    Route::post('/missing-attendances/{attendance}/approved', 'ApprovedAttendancesController@store')->name('approved-attendances.store');
 
 
-Route::get('/enrollments/classes', 'StudentsEnrollmentController@create')->name('enrollments.create');
-Route::post('/classes/enrollments', 'StudentsEnrollmentController@store')->name('enrollments.store');
-Route::get('/classes/enrollments/{class}/{student}', 'StudentsEnrollmentController@edit')->name('enrollments.edit');
-Route::patch('/classes/enrollments/{class}', 'StudentsEnrollmentController@update')->name('enrollments.update');
-Route::delete('/classes/enrollments/{class}', 'StudentsEnrollmentController@destroy')->name('enrollments.destroy');
+    Route::get('/enrollments/classes', 'StudentsEnrollmentController@create')->name('enrollments.create');
+    Route::post('/classes/enrollments', 'StudentsEnrollmentController@store')->name('enrollments.store');
+    Route::get('/classes/enrollments/{class}/{student}', 'StudentsEnrollmentController@edit')->name('enrollments.edit');
+    Route::patch('/classes/enrollments/{class}', 'StudentsEnrollmentController@update')->name('enrollments.update');
+    Route::delete('/classes/enrollments/{class}', 'StudentsEnrollmentController@destroy')->name('enrollments.destroy');
 
-Route::resource('subjects', 'SubjectsController');
+    Route::resource('subjects', 'SubjectsController');
+});
